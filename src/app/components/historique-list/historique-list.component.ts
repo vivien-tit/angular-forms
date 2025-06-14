c2993d52c0d5c51189566f825419d71bb43bbd2b
vivien <vtitsop4@gmail.com>
2025-06-14 16:24:53 +0100

    mise a jour du projet

--- src/app/components/historique-list/historique-list.component.ts
+++ src/app/components/historique-list/historique-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-historique-list',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './historique-list.component.html',
   styleUrl: './historique-list.component.css'
 })
-export class HistoriqueListComponent {
+export class HistoriqueListComponent implements OnInit {
+  historiqueList: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadHistorique();
+    console.log('document recu', this.historiqueList);
+  }
+
+  loadHistorique(): void {
+    this.http.get<any[]>('http://localhost:8080/api/historiques')
+      .subscribe(data => this.historiqueList = data);
+  }    
 
 }
